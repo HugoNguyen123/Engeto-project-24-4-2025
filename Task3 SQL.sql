28ec56723ccceeb13ac7fdced1f6cc23c51ca6b3
HugoNguyen123 <xrakosnicek123x@gmail.com>
2025-11-17 17:04:58 +0100

    Refactor SQL query to analyze food price growth

--- Task3 SQL.sql
+++ Task3 SQL.sql
@@ -1,47 +1,20 @@
-#4) Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+#3) Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 
-WITH mzdy AS (
+WITH growth AS (
     SELECT 
-        t1.payroll_year,
-        ROUND(((t2.salary - t1.salary) / t1.salary) * 100, 2) AS yoy_mzdy
-    FROM (
-        SELECT payroll_year, AVG(salary) AS salary
-        FROM t_huu_viet_nguyen_project_SQL_primary_final
-        WHERE ib_code IS NOT NULL
-        GROUP BY payroll_year
-    ) t1
-    JOIN (
-        SELECT payroll_year, AVG(salary) AS salary
-        FROM t_huu_viet_nguyen_project_SQL_primary_final
-        WHERE ib_code IS NOT NULL
-        GROUP BY payroll_year
-    ) t2
-        ON t2.payroll_year = t1.payroll_year + 1
-),
-
-potraviny AS (
-    SELECT 
-        t1.payroll_year,
-        ROUND(AVG(((t2.value / t1.value) - 1) * 100), 2) AS yoy_potraviny
+        t1.food_code,
+        t1.food_name,
+        ROUND(((t2.value::numeric / t1.value::numeric) - 1) * 100, 2) AS yoy_growth
     FROM t_huu_viet_nguyen_project_SQL_primary_final t1
     JOIN t_huu_viet_nguyen_project_SQL_primary_final t2
-        ON t2.food_code = t1.food_code
-        AND t2.payroll_year = t1.payroll_year + 1
-    GROUP BY t1.payroll_year
+        ON t2.payroll_year = t1.payroll_year + 1
+        AND t1.food_code = t2.food_code
 )
-
 SELECT 
-    p.payroll_year AS year,
-    p.yoy_potraviny,
-    m.yoy_mzdy,
-    (p.yoy_potraviny - m.yoy_mzdy) AS rozdil,
-    CASE WHEN p.yoy_potraviny - m.yoy_mzdy > 10 
-         THEN 'ANO – růst cen > růst mezd o více než 10 %'
-         ELSE 'NE'
-    END AS extremni_narust
-FROM potraviny p
-JOIN mzdy m USING (payroll_year)
-ORDER BY year;
-
-
+    g.food_code,
+    g.food_name,
+    ROUND(AVG(g.yoy_growth::numeric), 2) AS avg_yoy_growth_percentage
+FROM growth g
+GROUP BY g.food_code, g.food_name
+ORDER BY avg_yoy_growth_percentage;
 
