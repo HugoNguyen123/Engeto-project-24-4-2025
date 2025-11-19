26ee051d5621ff2dae73c7d026c2264a96cd1a01
HugoNguyen123 <xrakosnicek123x@gmail.com>
2025-11-19 20:49:39 +0100

    Update Task4 SQL.sql

--- Task4 SQL.sql
+++ Task4 SQL.sql
@@ -1,24 +1,19 @@
 #4) Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
 
-WITH mzdy AS (
+WITH prum_mzdy AS (
+    SELECT payroll_year, AVG(salary) AS salary
+    FROM t_huu_viet_nguyen_project_SQL_primary_final
+    WHERE ib_code IS NOT NULL
+    GROUP BY payroll_year
+),
+mzdy AS (
     SELECT 
         t1.payroll_year,
         ROUND(((t2.salary - t1.salary) / t1.salary) * 100, 2) AS yoy_mzdy
-    FROM (
-        SELECT payroll_year, AVG(salary) AS salary
-        FROM t_huu_viet_nguyen_project_SQL_primary_final
-        WHERE ib_code IS NOT NULL
-        GROUP BY payroll_year
-    ) t1
-    JOIN (
-        SELECT payroll_year, AVG(salary) AS salary
-        FROM t_huu_viet_nguyen_project_SQL_primary_final
-        WHERE ib_code IS NOT NULL
-        GROUP BY payroll_year
-    ) t2
+    FROM prum_mzdy t1
+    JOIN prum_mzdy t2
         ON t2.payroll_year = t1.payroll_year + 1
 ),
-
 potraviny AS (
     SELECT 
         t1.payroll_year,
@@ -29,19 +24,23 @@ potraviny AS (
         AND t2.payroll_year = t1.payroll_year + 1
     GROUP BY t1.payroll_year
 )
-
 SELECT 
     p.payroll_year AS year,
     p.yoy_potraviny,
     m.yoy_mzdy,
     (p.yoy_potraviny - m.yoy_mzdy) AS rozdil,
-    CASE WHEN p.yoy_potraviny - m.yoy_mzdy > 10 
-         THEN 'ANO – růst cen > růst mezd o více než 10 %'
-         ELSE 'NE'
-    END AS extremni_narust
+    ROUND(10 - (p.yoy_potraviny - m.yoy_mzdy), 2) AS procent_od_hranice,
+    CASE 
+        WHEN (p.yoy_potraviny - m.yoy_mzdy) > 10 THEN 'ANO – růst cen > růst mezd o více než 10 %'
+        WHEN (p.yoy_potraviny - m.yoy_mzdy) BETWEEN 8 AND 10 THEN 'BLÍZKO HRANICI 10 %'
+        WHEN (p.yoy_potraviny - m.yoy_mzdy) BETWEEN 6 AND 8 THEN 'MÍRNĚ ZVÝŠENÝ NÁRŮST (6–8 %)'
+        ELSE 'NE – růst cen méně než mezd + 6 %'
+    END AS kategorie_narustu
 FROM potraviny p
-JOIN mzdy m USING (payroll_year)
-ORDER BY year;
+JOIN mzdy m
+    ON p.payroll_year = m.payroll_year
+ORDER BY p.payroll_year;
+
 
 
 	
